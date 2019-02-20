2a8eff500fe3657e3e6a206f87d6fab70bc53035
d051a <akargavin@gmail.com>
2019-02-20 20:48:06 +0300

    20/01/2019

--- marketplace/forms.py
+++ marketplace/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Stuff
+from django import forms
+from .models import Stuff, City, SubCategory
 
 
-class SearchForm(ModelForm):
-    class Meta:
-        model = Stuff
-        fields = ['city', 'stuff_subcategory', 'tags', ]
+class SearchForm(forms.Form):
+    city = forms.ModelChoiceField(label='Город', queryset=City.objects.all())
+    stuff_subcategory = forms.ModelChoiceField(label='Категория', queryset=SubCategory.objects.all())
+    data_start = forms.DateField(label='Дата от')
+    data_finish = forms.DateField(label='до')
+    price_start = forms.DateField(label='Цена от')
+    price_finish = forms.DateField(label='до')
+    tags = forms.CharField(label='Тэги')
